15b4a22b9dca2087128a983f233c08000a58df3e
kelton <kelton491@gmail.com>
2023-01-21 18:24:07 -0300

    alteração

--- PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/atividadesjava/BoletinEstudantil.java
+++ PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/atividadesjava/BoletinEstudantil.java
@@ -1,14 +1,16 @@
+package edu.kelton.atividadesjava;
+
 public class BoletinEstudantil {
     public static void main(String[] args) {
         
         int mediaFinal = 6;
-        
-        if(mediaFinal<6)
+ 
+        if (mediaFinal <6 )
             System.out.println("Reprovado");
-            else if(mediaFinal == 6)
-                System.out.println("Prova Minerva");
-                    else
-                        System.out.println("APROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("Prova Minerva");
+         else
+            System.out.println("APROVADO");
 
     }
-}
+}   
--- PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/atividadesjava/MinhaClass.java
+++ PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/atividadesjava/MinhaClass.java
@@ -1,3 +1,4 @@
+package edu.kelton.atividadesjava;
 public class MinhaClass {
 
     public static void main (String [] args) {
