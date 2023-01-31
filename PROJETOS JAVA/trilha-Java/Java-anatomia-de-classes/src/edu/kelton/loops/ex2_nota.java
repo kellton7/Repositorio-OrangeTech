f163564ac7fa1a19e82ab9c80f5be63d352a4d91
kelton <kelton491@gmail.com>
2023-01-31 20:50:40 -0300

    Utilizando While

--- PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/loops/ex2_nota.java
+++ PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/loops/ex2_nota.java
@@ -3,6 +3,7 @@ package edu.kelton.loops;
 import java.util.Scanner;
 public class ex2_nota{
     public static void main(String[] args) {
+        
         Scanner scan = new Scanner(System.in);
 
         int nota;
