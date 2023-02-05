2fbcb23110f54d570e123361dc3a893b442c0922
kelton <kelton491@gmail.com>
2023-02-05 20:06:23 -0300

    Matriz 4x4

--- PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/loops/ex10_arrayMulti.java
+++ PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/loops/ex10_arrayMulti.java
@@ -1,5 +1,26 @@
 package edu.kelton.loops;
 
+import java.util.Random;
+
 public class ex10_arrayMulti {
-    
+    public static void main(String[] args) {
+        Random random =new Random();
+
+        int [][] M = new int [4][4];
+        
+        for(int l = 0; l < M.length ; l++){
+
+            for(int c =0; c < M[l].length; c++){
+                M[l][c] = random.nextInt(9);
+            }
+        }
+
+        System.out.println("Matriz: ");
+        for (int[] linha : M ) {
+            for (int coluna : linha ) {
+                System.out.print(coluna + " ");
+        }
+        System.out.println(" ");
+    }
+}
 }
