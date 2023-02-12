ac0184c7a3a56f8dfef6ec5cd7209b38c899895e
kelton <kelton491@gmail.com>
2023-02-12 09:56:53 -0300

    desafio 04 do bootcamp da inter

---
+++ PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/bin/edu/kelton/DesafiosDio/desafio04.java
@@ -0,0 +1,14 @@
+package edu.kelton.DesafiosDio;
+import java.util.Scanner;
+
+public class desafio04 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int doce = leitor.nextInt();
+        int total = doce * 2;
+        
+        System.out.println("O cliente obteve " + total + " doces");
+        // TODO: Crie a condição necessária para que cada cliente saiba quantos doces vai obter de acordo com a entrada
+
+    }
+}
