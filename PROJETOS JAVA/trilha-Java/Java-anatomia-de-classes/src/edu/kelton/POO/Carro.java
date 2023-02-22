0356f2cea7ad53740dd4b8a2fe85f6e5e80a2a2c
kelton <kelton491@gmail.com>
2023-02-22 11:44:45 -0300

    Update

--- PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/POO/Carro.java
+++ PROJETOS JAVA/trilha-Java/Java-anatomia-de-classes/src/edu/kelton/POO/Carro.java
@@ -4,6 +4,48 @@ class Carro {
 
     String cor;
     String modelo;
-    float capacidadeTanque;
+    int capacidadeTanque;
 
+    Carro(){
+    }
+
+    Carro(String cor, String modelo, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    // métodos set e get
+
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    String getCor(){
+        return cor;
+    }
+
+     void setModelo(String modelo){
+        this.modelo = modelo;
+     }
+
+     String getModelo(){
+        return modelo;
+     }
+
+     // set e ger da capacidade do  tanque
+
+    void setCapacidadeTanque(int capacidadeTanque){
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getCapacidadeTanque(){
+        return capacidadeTanque;
+    }
+
+    //método para encher o tanque
+
+    double totalValorTanque(double valorCombustivel){
+        return capacidadeTanque * valorCombustivel;
+    }
 }
